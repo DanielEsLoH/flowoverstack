c91625091aa167018ee0ff530dc0b632c42d5a73
Daniel Lonodoño <daniel.esloh@gmail.com>
2023-11-01 21:06:59 -0500

    Validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ApplicationRecord
+    validates :title, presence: true
+    validates :description, presence: true
     belongs_to :user
     has_many :comments, as: :commentable, dependent: :destroy
 end
