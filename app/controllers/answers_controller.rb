54da52b7d91e07f389d0e53f66fd3d4bc74b25d2
Daniel Lonodoño <daniel.esloh@gmail.com>
2023-12-15 21:36:51 -0500

    Add notifications to answers controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,15 +4,19 @@ class AnswersController < ApplicationController
     @answer = @question.answers.build(answer_params)
     @answers = @question.answers
 
-    if @answer.save
-      @votes = @answer.votes
-      flash[:success] = "Respuesta creada con exito"
-      respond_to do |format|
-        format.turbo_stream { render turbo_stream: turbo_stream.replace("answers_all", partial: 'answers/answers_all', locals: {answers: @answers}) }
+    respond_to do |format|
+      if @answer.save
+        @votes = @answer.votes
+        flash[:notice] = "¡Respuesta recibida!"
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.append('flash-messages', partial: 'shared/notifications', locals: {message: flash[:notice]}) +
+                              turbo_stream.replace("answers_all", partial: 'answers/answers_all', locals: {answers: @answers})
+        end
+      else
+        flash[:alert] = "Error al crear la respuesta"
+        format.turbo_stream { render turbo_stream: turbo_stream.append('flash-messages', partial: 'shared/notifications', locals: {message: flash[:alert]})}
+        format.html { redirect_to question_path(@question) }
       end
-    else
-      flash[:error] = "Error al crear la respuesta"
-      format.turbo_stream { render turbo_stream: turbo_stream.replace("answers_all", partial: 'answers/answers_all', locals: {answers: @answers}) }
     end
   end
 
